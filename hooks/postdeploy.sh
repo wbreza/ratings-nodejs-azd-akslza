c40ebb4882665e40b9bec437ee2f43f1568f262b
Wallace Breza <wallace.breza@microsoft.com>
2023-08-07 13:58:23 -0700

    Updates azd provision/deploy hook scripts

--- hooks/postdeploy.sh
+++ hooks/postdeploy.sh
@@ -6,18 +6,9 @@ if [[ ! -z "$CI" ]]; then
    exit
 fi
 
-export NAMESPACE=ratingsapp
 
 docker rmi ${SERVICE_API_IMAGE_NAME}
 docker rmi ${SERVICE_WEB_IMAGE_NAME}
 
-INGRESS_IP=$(kubectl get ingress -n ${NAMESPACE} ratings-web-https -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 PIP_NAME=$(az network application-gateway show -g ${AZURE_RESOURCE_GROUP} -n ${AZURE_APP_GATEWAY_NAME} --query "frontendIPConfigurations[].publicIPAddress[].id" -o tsv|sed 's/\/.*\///g')
-
 az network public-ip update -g ${AZURE_RESOURCE_GROUP} -n $PIP_NAME --dns-name ${AZURE_DNS_LABEL}
-
-echo "==================================================================================="
-echo "===  Please was a few minutes for the new fqdn to propogate through dns and then "
-echo "===  You may access the application at "
-echo "===  https://${AZURE_DNS_LABEL}.${AZURE_LOCATION}.cloudapp.azure.com"
-echo "==================================================================================="
--- hooks/postprovision.sh
+++ hooks/postprovision.sh
@@ -6,19 +6,13 @@ if [[ ! -z "$CI" ]]; then
    exit
 fi
 
-SIGNED_IN_USER=$(az ad signed-in-user show --query id -o tsv)
-AKS_ID=$(az aks show --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_AKS_CLUSTER_NAME --query id -o tsv)
-AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN='b1ff04bb-8a4e-4dc4-8eb5-8693973ce19b'
+# SIGNED_IN_USER=$(az ad signed-in-user show --query id -o tsv)
+# AKS_ID=$(az aks show --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_AKS_CLUSTER_NAME --query id -o tsv)
+# AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN='b1ff04bb-8a4e-4dc4-8eb5-8693973ce19b'
 
-if [[ -z "${SIGNED_IN_USER}" ]]; then
-   echo "Signed in user not found.  Skipping AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN role assignment."
-   exit
-fi
+# az role assignment create --role ${AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN} \
+#    --assignee-object-id ${SIGNED_IN_USER} --assignee-principal-type User --scope ${AKS_ID}
 
-az role assignment create --role ${AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN} \
-   --assignee-object-id ${SIGNED_IN_USER} --assignee-principal-type User --scope ${AKS_ID}
-
-az aks get-credentials --resource-group ${AZURE_RESOURCE_GROUP} --name ${AZURE_AKS_CLUSTER_NAME} --context ${AZURE_AKS_CLUSTER_NAME} --file=${HOME}/.kube/${AZURE_AKS_CLUSTER_NAME} --overwrite-existing --admin
-az aks get-credentials --resource-group ${AZURE_RESOURCE_GROUP} --name ${AZURE_AKS_CLUSTER_NAME} --context ${AZURE_AKS_CLUSTER_NAME} --overwrite-existing --admin
-azd env set KUBECONFIG ${HOME}/.kube/${AZURE_AKS_CLUSTER_NAME}
-export KUBECONFIG=${HOME}/.kube/${AZURE_AKS_CLUSTER_NAME}
+kubelogin convert-kubeconfig -l azurecli
+kubectl config current-context
+kubectl get service
